58955a9288cef30b9234d43648aa6f639f5f4e75
Kemal Dwi Heldy Muhammad <21bigbozz09@gmail.com>
2026-01-07 23:31:24 +0700

    asd

--- processor/inference/scm_pipeline_v1.py
+++ processor/inference/scm_pipeline_v1.py
@@ -215,6 +215,11 @@ def scm_pose_callback(element, buffer, user_data):
             print(" Object label:", object.get_label())
             print("  BBox:", object.get_bbox())
             print("  Confidence:", object.get_confidence())
+
+            bbox = object.get_bbox()
+            bbox_str = f"({bbox.xmin():.2f}, {bbox.ymin():.2f}), ({bbox.xmax():.2f}, {bbox.ymax():.2f})"
+            print("  BBox coords:", bbox_str)
+
             print("\n\n")
 
         detections = roi.get_objects_typed(hailo.HAILO_DETECTION)
